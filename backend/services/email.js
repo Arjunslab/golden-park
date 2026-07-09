fe5a17e7e7adc217cac5ccf02e7e58a2390b86dc
Arjunslab <arjunb5454@gmail.com>
2026-07-09 21:12:43 +0530

    fixing dotenv

--- backend/services/email.js
+++ backend/services/email.js
@@ -1,6 +1,4 @@
 import { Resend } from "resend";
-import { getWelcomeTemplate, getOtpTemplate } from "../emails/templates.js";
-import { Dotenv } from "dotenv";
 
 const resend = new Resend(process.env.RESEND_KEY);
 
